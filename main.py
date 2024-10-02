cb1afba26928d4babd5bfa89ae9db93331ef665a
lauralstephenson <lstephenson556@gmail.com>
2024-10-02 17:27:43 +0900

    This uploads the folder to GitHub.

---
+++ main.py
@@ -0,0 +1,70 @@
+print('''
+
+*******************************************************************************
+
+          |                   |                  |                     |
+
+_________|________________.=""_;=.______________|_____________________|_______
+
+|                   |  ,-"_,=""     `"=.|                  |
+
+|___________________|__"=._o`"-._        `"=.______________|___________________
+
+          |                `"=._o`"=._      _`"=._                     |
+
+_________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+
+_________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+
+/______/______/______/______/______/______/______/______/______/______/_____ /
+
+*******************************************************************************
+
+''')
+
+print("Welcome to Treasure Island.")
+
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+first_step = input(
+    "Where would you like to go? Type 'left' or 'right'.").lower()
+if first_step == "right" and first_step != "left":
+  print("Fall in a hole. Game Over.")
+elif first_step == "left":
+  second_step = input(
+      " Do you want to swim or wait? Type your choice now.").lower()
+if second_step == "swim" or second_step != "wait":
+  print("You were ttacked by a giant trout. Game Over.")
+third_step = input("Which door color has the treasure?").lower()
+if third_step == "red":
+  print("Burned by fire. Game Over.")
+elif third_step == "blue":
+  print("Eaten by beasts. Game Over.")
+elif third_step == "yellow":
+  print("You win!")
+else:
+  print("Game over!")
